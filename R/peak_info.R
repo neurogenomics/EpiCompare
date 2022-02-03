1bc5916676541aaeefae8e022760252bb162191f
Sera Choi <serachoi1230@gmail.comwq>
2022-02-03 15:12:14 +0000

    added tidy_peakfiles to standardise input peakfiles

--- R/peak_info.R
+++ R/peak_info.R
@@ -22,10 +22,10 @@
 #'
 peak_info <- function(peak_list, file_names, blacklist){
   # for each peakfile retrieve the number of peaks and store in list
-  Total_N <- c()
+  peakN_before_tidy <- c()
   for (sample in peak_list){
     N <- length(sample)
-    Total_N <- c(Total_N, N)
+    peakN_before_tidy <- c(peakN_before_tidy, N)
   }
   # for each peakfile calculate the percentage of overlapping peaks with
   # blacklisted region
@@ -35,8 +35,27 @@ peak_info <- function(peak_list, file_names, blacklist){
     blacklistP <- blacklistN/length(sample)*100
     blacklist_percent <- c(blacklist_percent, signif(blacklistP, 3))
   }
+  # for each peakfile calculate percentage of non-standard and mitochondrial chromosome
+  tidy_percent <- c()
+  for (sample in peak_list){
+    tidy_peak <- BRGenomics::tidyChromosomes(sample, keep.X = TRUE, keep.Y = TRUE)
+    removedN <- length(sample) - length(tidy_peak)
+    tidy_peak_percent <- removedN/length(sample)*100
+    tidy_percent <- c(tidy_percent, signif(tidy_peak_percent, 3))
+  }
+  # remove blacklisted regions and non-standard chromosomes
+  peaklist_tidy <- tidy_peakfile(peaklist = peak_list,
+                                 blacklist = blacklist)
+
+  # after tidying, retrieve the number of peaks and store in list
+  peakN_after_tidy <- c()
+  for (sample in peaklist_tidy){
+    N <- length(sample)
+    peakN_after_tidy <- c(peakN_after_tidy, N)
+  }
+
   # combine two metrics into a data frame
-  df <- data.frame(file_names, Total_N, blacklist_percent)
-  colnames(df) <- c("Sample", "Total_N", "Blacklisted_Peaks (%)")
+  df <- data.frame(file_names, peakN_before_tidy, blacklist_percent, tidy_percent, peakN_after_tidy)
+  colnames(df) <- c("Sample", "PeakN before tidy", "Blacklisted peaks removed (%)", "Non-standard peaks removed (%)", "PeakN after tidy")
   return(df)
 }
---
+++ R/tidy_peakfile.R
@@ -0,0 +1,29 @@
+#' Tidy peakfiles in GRanges
+#'
+#' @param peaklist A list of peak files as GRanges object. Objects in lists using `list()`.
+#' @param blacklist Peakfile specifying blacklisted regions as GRanges object.
+#'
+#' @return list of GRanges object
+#' @export
+#'
+#' @examples
+#' library(EpiCompare)
+#' data("encode_H3K27ac") # example dataset as GRanges object
+#' data("CnT_H3K27ac") # example dataset as GRanges object
+#' data("hg19_blacklist") # blacklist region for hg19 genome
+#'
+#' tidy_peakfile(peaklist = list(encode_H3K27ac, CnT_H3K27ac),
+#'                      blacklist = hg19_blacklist)
+#'
+tidy_peakfile <- function(peaklist, blacklist){
+  # for each peakfile, remove peaks in blacklisted region
+  # and remove non-standard and mitochondrial chromosomes
+  peaklist_tidy <- list()
+  for(sample in peaklist){
+    blacklist_removed <- IRanges::subsetByOverlaps(sample, blacklist, invert = TRUE)
+    blacklist_removed_tidy <- BRGenomics::tidyChromosomes(blacklist_removed, keep.X = TRUE, keep.Y = TRUE)
+    peaklist_tidy <- c(peaklist_tidy, blacklist_removed_tidy)
+  }
+  return(peaklist_tidy)
+}
+
