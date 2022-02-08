57ca83bb175e6e70fdcbfb2e2862235a1976eb03
Sera Choi <serachoi1230@gmail.comwq>
2022-02-08 17:06:02 +0000

    updated markdown

--- R/overlap_stat_plot.R
+++ R/overlap_stat_plot.R
@@ -2,8 +2,8 @@
 #'
 #' This function calculates the statistical significance of overlapping/non-overlapping peaks against a reference peak file.
 #' If the reference peak file has the BED6+4 format (peak called by MACS2), the function generates a series of boxplots showing
-#' the distribution of q-values for overlapping and non-overlapping peaks for each sample.
-#' If the reference peak file does not have the BED6+4 format, the function uses `enrichPeakOverlap()` from ChIPseeker package
+#' the distribution of q-values for sample peaks that are overlapping and non-overlapping with the reference.
+#' If the reference peak file does not have the BED6+4 format, the function uses `enrichPeakOverlap()` from `ChIPseeker` package
 #' to calculate the statistical significance of overlapping peaks only.
 #'
 #' @param reference A reference peak file as GRanges object.
@@ -27,43 +27,42 @@
 overlap_stat_plot <- function(reference, peaklist, namelist){
   ## check if the file has BED6+4 format
   if(ncol(reference@elementMetadata) == 7){
-    df <- NULL
+    main_sample_df <- NULL
     # for each peakfile, obtain overlapping and unique peaks
     for (i in 1:length(peaklist)){
-      overlap <- IRanges::subsetByOverlaps(x = reference, ranges = peaklist[[i]])
-      unique <- IRanges::subsetByOverlaps(x = reference, ranges = peaklist[[i]], invert = TRUE)
+      # reference peaks found in sample peaks
+      sample_overlap <- IRanges::subsetByOverlaps(x = reference, ranges = peaklist[[i]])
+      sample_unique <- IRanges::subsetByOverlaps(x = reference, ranges = peaklist[[i]], invert = TRUE)
       # if no overlap, set q-value as 0 to avoid error
-      # else, obtain q-value from V9 column
-      if (length(overlap) == 0){
-        qvalue <- 0
+      # else, obtain q-value from field V9
+      if (length(sample_overlap) == 0){
+        sample_overlap_qvalue <- 0
+      }else if(length(sample_unique) == 0){
+        sample_unique_qvalue <- 0
       }else{
-        qvalue <- overlap$V9}
+        sample_overlap_qvalue <- sample_overlap$V9
+        sample_unique_qvalue <- sample_unique$V9
+      }
       # create data frame of q-values for overlapping peaks
       sample <- namelist[[i]]
       group <- "overlap"
-      overlap_df <- data.frame(qvalue, sample, group)
-      # if no unique peaks, set q-value as 0 to avoid error
-      # else, obtain 1-values from V9 column
-      if (length(unique) == 0){
-        qvalue <- 0
-      }else{
-        qvalue <- unique$V9}
+      sample_overlap_df <- data.frame(sample_overlap_qvalue, sample, group)
+      colnames(sample_overlap_df) <- c("qvalue", "sample", "group")
       # create data frame of q-values for unique peaks
-      sample <- namelist[[i]]
       group <- "unique"
-      unique_df <- data.frame(qvalue, sample, group)
+      sample_unique_df <- data.frame(sample_unique_qvalue, sample, group)
+      colnames(sample_unique_df) <- c("qvalue", "sample", "group")
       # combine two data frames
-      overlap_unique_df <- rbind(overlap_df, unique_df)
-      df <- rbind(df, overlap_unique_df)
+      sample_df <- rbind(sample_overlap_df, sample_unique_df)
+      main_sample_df <- rbind(main_sample_df, sample_df)
     }
-    # create paired boxplot for each peak file
-    plot <- ggplot2::ggplot(df, ggplot2::aes(x=sample, y=qvalue, fill=group)) +
-            ggplot2::geom_boxplot(outlier.shape = NA) +
-            ggplot2::theme_light() + ggplot2::ylim(0, 500) +
-            ggplot2::labs(x="",y="-log10(q)",fill="") +
-            ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, vjust = 1, hjust=1))
-
-    return(list(plot, df))
+    # create paired boxplot for each peak file (sample)
+    sample_plot <- ggplot2::ggplot(main_sample_df, ggplot2::aes(x=sample, y=qvalue, fill=group)) +
+                   ggplot2::geom_boxplot(outlier.shape = NA) +
+                   ggplot2::theme_light() + ggplot2::ylim(0, 500) +
+                   ggplot2::labs(x="",y="-log10(q)",fill="") +
+                   ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, vjust = 1, hjust=1))
+    return(list(sample_plot, main_sample_df))
     # for files not in BED6+4 format
     }else{
       # calculate significance of overlapping peaks using enrichPeakOverlap()
@@ -82,7 +81,8 @@ overlap_stat_plot <- function(reference, peaklist, namelist){
       plot <- ggplot2::ggplot(data=overlap_result, ggplot2::aes(x=tSample,y=percent_overlap,fill=p.adjust)) +
               ggplot2::geom_bar(stat="identity") + ggplot2::scale_fill_continuous(type="viridis") +
               ggplot2::theme_light() + ggplot2::labs(x="",y="Percentage overlap (%)") +
-              ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, vjust = 1, hjust=1))
+              ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, vjust = 1, hjust=1)) +
+              ggplot2::ylim(0,100)
     # return both plot and data frame
     return(list(plot, overlap_result))
   }
