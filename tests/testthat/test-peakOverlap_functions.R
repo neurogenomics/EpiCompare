4bc08b926fdd232da428988564275d38263aa16c
Sera Choi <serachoi1230@gmail.comwq>
2022-04-04 14:07:46 +0100

    updated tests

--- tests/testthat/test-peakOverlap_functions.R
+++ tests/testthat/test-peakOverlap_functions.R
@@ -11,8 +11,8 @@ testthat::test_that("overlap_heatmap() generates correct plot
                     when interact=T/F", {
   p_interact <- EpiCompare::overlap_heatmap(peaklist, interact = TRUE)
   p <- EpiCompare::overlap_heatmap(peaklist, interact = FALSE)
-  testthat::expect_true(methods::is(p_interact,c("plotly","htmlwidget")))
-  testthat::expect_true(methods::is(p,c("gg","ggplot")))
+  testthat::expect_true(methods::is(p_interact,"plotly"))
+  testthat::expect_true(methods::is(p,"ggplot"))
 })
 
 # test upset_plot()
@@ -29,7 +29,7 @@ testthat::test_that("overlap_stat_plot() generates correct output", {
   out <- EpiCompare::overlap_stat_plot(reference, peaklist)
   p <- out[[1]]
   df <- out[[2]]
-  testthat::expect_true(methods::is(p,c("gg","ggplot")))
+  testthat::expect_true(methods::is(p,"ggplot"))
   testthat::expect_equal(unique(out[[2]]$sample),c("encode","CnT"))
   testthat::expect_equal(unique(out[[2]]$group),c("overlap","unique"))
 })
